84eb97518b6157b0b35e849ecb8d212a925c39fa
nekosyougun <sakasitamasaru@gmail.com>
2021-11-11 17:45:48 +0900

    tweetsDBの更新

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,24 @@ ActiveRecord::Schema.define(version: 2021_10_21_062913) do
     t.text "explanatory_text", null: false
     t.integer "game_model_id", null: false
     t.integer "genre_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "nickname"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "tweets", "users"
 end
