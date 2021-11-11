b0e0ffa1f6f7ae7d9b0ef13adbcdf8dbb1a9703f
nekosyougun <sakasitamasaru@gmail.com>
2021-11-11 17:43:20 +0900

    tweetsコントローラーの修正3

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @tweets = Tweet.all
@@ -31,10 +32,16 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require(:tweet).permit(:name, :image, :explanatory_text)
+    params.require(:tweet).permit(:title, :image, :explanatory_text)
   end
 
   def set_tweet
     @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
